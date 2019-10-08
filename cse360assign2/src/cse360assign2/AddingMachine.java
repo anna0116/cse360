778a496f742d385ea7a5bb39512fe2a28de5de85
liza1 <liza1@DESKTOP-GSLK71H.3500.dhcp.asu.edu>
2019-10-08 13:42:01 -0700

    This contains the project with empty method in AddingMachine.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,46 @@
+/**
+ * Assignment 2
+ * Author : Anna Maria Paul Mundadan
+ * Class  : CSE 360 (Wednesday session)
+ * Date   : 10-09-2019
+ * File   : AddingMachine.java
+ * Purpose: Initial version of AddingMachine with empty methods
+ */
+
+package cse360assign2;
+
+public class AddingMachine {
+
+	//VARIABLE
+	private int total;
+	
+	//DEFAULT CONSTRUCTOR
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//METHOD TO CALCULATE THE TOTAL
+	public int getTotal () {
+		return 0;
+	}
+	
+	//METHOD TO COMPUTE THE SUM
+	public void add (int value) {
+		
+	}
+	
+	//METHOD TO SUBTRACT
+	public void subtract (int value) {
+		
+	}
+		
+	//METHOD TO CONVERT INTO A STRING
+	public String toString () {
+		return "";
+	}
+
+	//METHOD TO CLEAR THE ENTRIES
+	public void clear() {
+	
+	}
+}
